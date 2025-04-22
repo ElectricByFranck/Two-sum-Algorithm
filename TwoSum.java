d2967322d44587db62a35dec4adb1d90fe0a8668
ElectricByFranck <fnana020@uottawa.ca>
2025-04-22 19:56:28 -0400

    Handling some error cases
    I tried to handle the case the target is not found by using the built-in toString() function. It's now possible to print the empty array if the target isn't found.

--- TwoSum.java
+++ TwoSum.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class TwoSum {
 
     //Constructor
@@ -27,16 +28,24 @@ public class TwoSum {
     public static void main(String[]args){
 
         //Creating an array with values
-        int[] tab1 = new int[]{1,2,3,4,5,6,7,8,9,10};
+        int[] tab1 = new int[]{1,2,3,4,5,6,7,8,9,10,11};
 
         //Instanciating the class
         TwoSum essai = new TwoSum();
 
         //Calling the methode with the needed arguments
-        int[] result = essai.Solution(tab1,10);
+        int[] result = essai.Solution(tab1,-10);
 
-        //Printing out the result of the method
-        System.out.println("[" + result[0] + "," + result[1]+ "]");
+        //Printing out the result of the method has found the target
+        if(result.length>0){
+            System.out.println("Target has been found !! -->  " +"[" + result[0] + "," + result[1]+ "]");
+        }
+
+        //Printing out this message if the method hasn't found the target
+        else{
+            System.out.println("Target hasn't been found !! -->  " + Arrays.toString(result));
+        }
+        
 
     }
 }
